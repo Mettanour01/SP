71cf0b2ee5bfdb7e1820bac505b2ce3250b22cb4
Mettanour <94889229+Mettanour01@users.noreply.github.com>
2021-12-02 10:59:52 +0300

    Sorting

--- Sorting.java
+++ Sorting.java
@@ -1,18 +1,21 @@
+
 public class Main {
     public static void main(String[] args) {
-        int mass[] = new int[]{2, 5, 2, 8, 5, 6, 8, 8};
-        for (int i = mass.length - 1; i > 0; i--) {
-            for (int j = 0; j < i; j++) {
-                if (mass[j] < mass[j+1]) {
-                    int tmp = mass[j];
-                    mass[j] = mass[j+1];
-                    mass[j+1] = tmp;
+        int[] array = {2, 4, 7, 6, 5, 1, 9, 3, 8};
+        bubbleSort(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static void bubbleSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int k = array.length - 1; k > i; k--) {
+                if (array[k - 1] > array[k]) {
+                    int tmp = array[k - 1];
+                    array[k - 1] = array[k];
+                    array[k] = tmp;
                 }
             }
         }
-        for (Integer m : mass){
-            System.out.print(m + " ");
-        }
-        System.out.println();
     }
 }
+
